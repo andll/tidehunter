5851584051045ebdd892dd0987a9508923bfaa7e
andll <andll@danasoft.ws>
2024-08-18 20:09:44 -0700

    Wal reader/writer

--- src/wal.rs
+++ src/wal.rs
@@ -1,4 +1,4 @@
-use crate::crc::{CrcFrame, IntoBytesFixed};
+use crate::crc::{CrcFrame, CrcReadError, IntoBytesFixed};
 use memmap2::MmapMut;
 use minibytes::Bytes;
 use std::fs::{File, OpenOptions};
@@ -6,35 +6,41 @@ use std::io;
 use std::path::Path;
 
 pub struct Wal {
-    file: File,
-    wmap: MmapMut,
-    rmap: Bytes,
+    map: MmapMut,
+}
+
+pub struct WalReader {
+    map: Bytes,
 }
 
 impl Wal {
-    pub fn open(p: &impl AsRef<Path>, len: u64) -> io::Result<Self> {
+    pub fn open(p: &impl AsRef<Path>, len: u64) -> io::Result<(Self, WalReader)> {
         let file = OpenOptions::new()
             .read(true)
             .write(true)
             .create(true)
             .open(p)?;
         file.set_len(len)?;
-        let wmap = unsafe {
+        let map = unsafe {
             memmap2::MmapOptions::new()
                 .len(len as usize)
                 .map_mut(&file)?
         };
-        let rmap = unsafe { memmap2::MmapOptions::new().len(len as usize).map(&file)? };
-        let rmap = rmap.into();
-        Ok(Self { file, wmap, rmap })
+        let writer = Self { map };
+        let map = unsafe { memmap2::MmapOptions::new().len(len as usize).map(&file)? };
+        let map = map.into();
+        let reader = WalReader { map };
+        Ok((writer, reader))
     }
 
     pub fn write(&mut self, pos: usize, w: &PreparedWalWrite) {
-        self.wmap[pos..].copy_from_slice(w.frame.as_ref());
+        self.map[pos..].copy_from_slice(w.frame.as_ref());
     }
+}
 
-    pub fn read(&self, pos: u32) -> Bytes {
-        unimplemented!()
+impl WalReader {
+    pub fn read(&self, pos: u32) -> Result<Bytes, CrcReadError> {
+        CrcFrame::read_from_checked(&self.map, pos as usize)
     }
 }
 
