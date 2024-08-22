3070fb24421359961dcd54d3b4f5c8b78e2996fa
andll <andll@danasoft.ws>
2024-08-22 10:51:44 -0700

    Mutex speed test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use parking_lot::Mutex;
+use std::time::Instant;
+
 mod batch;
 mod config;
 mod control;
@@ -9,5 +12,18 @@ mod primitives;
 mod wal;
 
 fn main() {
-    println!("Hello, world!");
+    mutex_speed_test();
+}
+
+fn mutex_speed_test() {
+    const C: usize = 1_000_000;
+    let mut v = Vec::with_capacity(C);
+    for i in 0..C {
+        v.push(Mutex::new(i));
+    }
+    let start = Instant::now();
+    for m in &v {
+        *m.lock() += 1;
+    }
+    println!("Duration {:?}", start.elapsed());
 }
