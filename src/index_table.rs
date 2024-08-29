2074872d36965f1a1a244adf3043ea81fa9d331f
andll <andll@danasoft.ws>
2024-08-29 13:51:08 -0700

    Introduce IndexTable::make_dirty

--- src/index_table.rs
+++ src/index_table.rs
@@ -1,6 +1,7 @@
 use crate::wal::WalPosition;
 use minibytes::Bytes;
 use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
 
 #[derive(Default, Clone, Serialize, Deserialize)]
 pub(crate) struct IndexTable {
@@ -24,6 +25,7 @@ impl IndexTable {
         }
     }
 
+    /// Merges dirty IndexTable into a loaded IndexTable
     pub fn merge_dirty(&mut self, dirty: &Self) {
         // todo implement this efficiently taking into account both self and dirty are sorted
         for (k, v) in dirty.data.iter() {
@@ -32,6 +34,19 @@ impl IndexTable {
             } else {
                 self.insert(k.clone(), *v);
             }
+        }
+    }
+
+    /// Change loaded dirty IndexTable into unloaded dirty by retaining dirty keys and tombstones
+    pub fn make_dirty(&mut self, mut dirty_keys: HashSet<Bytes>) {
+        // todo this method can be optimized if dirty_keys are made sorted
+
+        // only retain keys that are dirty, removing all clean keys
+        self.data.retain(|(k, _)| dirty_keys.remove(k));
+        // remaining dirty_keys are not in this index, means they were deleted
+        // turn them into tombstones
+        for dirty_key in dirty_keys {
+            self.insert(dirty_key, WalPosition::INVALID);
         }
     }
 
