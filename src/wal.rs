68ac9160626f7f58b27b109922d257e0c8342f78
andll <andll@danasoft.ws>
2025-01-08 15:27:11 -0800

    Do not reader wal entry header for random reader

--- src/wal.rs
+++ src/wal.rs
@@ -257,15 +257,12 @@ impl Wal {
         if let Some(map) = self.get_map(map) {
             let offset = offset as usize;
             let header_end = offset + CrcFrame::CRC_HEADER_LENGTH;
-            let size = CrcFrame::read_size(&map.data[offset..header_end]);
-            let data = map.data.slice(header_end + inner_offset..header_end + size);
+            let data = map.data.slice(header_end + inner_offset..);
             Ok(WalRandomRead::Mapped(data))
         } else {
-            let mut buf = [0; CrcFrame::CRC_HEADER_LENGTH];
-            self.file.read_exact_at(&mut buf, pos.0)?;
-            let size = CrcFrame::read_size(&buf);
             let header_end = pos.0 + CrcFrame::CRC_HEADER_LENGTH as u64;
-            let range = (header_end + inner_offset as u64)..(header_end + size as u64);
+            let frag_end = self.layout.map_range(map).end;
+            let range = (header_end + inner_offset as u64)..frag_end;
             Ok(WalRandomRead::File(FileRange::new(&self.file, range)))
         }
     }
@@ -296,7 +293,9 @@ impl Wal {
                         .offset(range.start)
                         .len(self.layout.frag_size as usize);
                     if writeable {
-                        options/*.populate()*/.map_mut(&self.file)?.into()
+                        options /*.populate()*/
+                            .map_mut(&self.file)?
+                            .into()
                     } else {
                         options.map(&self.file)?.into()
                     }
